3f6859459e851bde6b2573cdc346fe75655958c2
cloud-daet <cloudsedgwickbalute.daet24@bicol-u.edu.ph>
2025-10-05 16:35:09 +0800

    Updated Arrays

---
+++ index.js
@@ -0,0 +1,11 @@
+// Initialize empty arrays for different categories
+let materials = [ // Array to hold materials
+  { name: "Detergent", qty: 20, price: 15 },
+  { name: "Fabric Conditioner", qty: 20, price: 5 },
+  { name: "Laundry Bag", qty: 20, price: 5 },
+];
+let cleanLaundry = []; // Array to hold clean laundry
+let uncleanLaundry = []; // Array to hold unclean laundry
+
+// Maximum weight limit for laundry in kg
+const MAX_WEIGHT = 7;
